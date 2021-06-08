c444f0f195472dd10013b46d69486dfc2b8a245f
Vítor Lemos <31148456+vitorglemos@users.noreply.github.com>
2021-06-08 07:55:32 -0300

    Added filter mode

--- prospective.py
+++ prospective.py
@@ -41,6 +41,11 @@ class ProspectiveProv(object):
         """Get all code components by trial_id"""
         return sqlite.code_components(self.conf)
 
+    def getSubscript(self, sqlite):
+        """Get all code components by trial_id"""
+        return sqlite.code_subscript(self.conf)
+
+
     def getFunction(self, sqlite):
         """Get all code of a function by name_def and trial_id"""
         return sqlite.code_function(self.conf)
@@ -52,7 +57,6 @@ class ProspectiveProv(object):
     def run(self):
         """Call modules and run prospectiveProv"""
         """ There are currently four filter types in prospective Prov.
-                lines: select code components based on start line passed as parameter: not active! 
                 partial: select code components based on start and last line: not active!
                 everything: select all code components: active!
                 function: select a specific function on script: active!
@@ -72,7 +76,6 @@ class ProspectiveProv(object):
             if status:
                 prov = DefinitionProvenanceAnalyzer(self.conf['trial_id'][0])
                 codes = {'lines': self.getComponents,
-                         'partial': self.getComponents,
                          'everything': self.getComponents,
                          'function': self.getFunction}
 
@@ -98,6 +101,20 @@ def main():
                         help="Insert the value 1 to show "
                              "all code activations in provenance graph.",
                         default=0)
+
+    parser.add_argument('--show', type=str, nargs='+', dest="show",
+                        help="--show s: show all script\n "
+                             "--show f <function_name>: Show diagram by function (using function name as search filter)"
+                             "--show l <line_start> <line_end>: Generate diagram from a script subset (using start "
+                             "line and end line as filter) "
+                             "Generate diagram from a script subset.",
+                        default='s')
+
+    parser.add_argument('--line', type=str, dest="line",
+                        help="Insert the start line and end line "
+                             "Generate diagram from a script subset.",
+                        default=False)
+
     parser.add_argument('--run', type=str, dest="runtime",
                         help="Insert the value 1 to show "
                              "the execution time of each code block.",
@@ -116,8 +133,25 @@ def main():
     activations = bool(argument.activation)
     checkpoints = bool(argument.runtime)
     contents = bool(argument.content)
+    filter_mode = argument.show
 
-    db = argument.database
+    start_line = 0
+    final_line = 0
+    name_def = None
+    provenance_types = "everything"
+
+    if len(filter_mode) > 0:
+        if filter_mode[0] == 's':
+            provenance_types = "everything"
+        if filter_mode[0] == 'f':
+            provenance_types, name_def = "function", filter_mode[1]
+        if filter_mode[0] == 'l':
+            provenance_types = "lines"
+            start_line = filter_mode[1]
+            final_line = filter_mode[2]
+
+
+
     if argument.trial[0] == "trial":
         trial_id = argument.trial[1]
     elif argument.trial[0].startswith("trial") == "trial":
@@ -126,15 +160,15 @@ def main():
         trial_id = argument.trial[0]
 
     db = argument.database
-    provenance_types = "everything"
-    name_def = 'crossing'
-
     data = defaultdict(list)
+
     data['database'].append(db)
     data['trial_id'].append(trial_id)
     data['indented'].append(indented)
     data['activations_v'].append(activations)
     data['checkpoints_v'].append(checkpoints)
+    data['start'].append(start_line)
+    data['final'].append(final_line)
     data['contents_v'].append(contents)
     data['filter_type'].append(provenance_types)
     data['def_name'].append(name_def)
